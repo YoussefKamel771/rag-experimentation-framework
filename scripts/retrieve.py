2e580a9771b80d98c558e3e6464797626507b129
Youssef <yousefkamel282@gmail.com>
2026-08-18 16:36:20 +0300

    edited retrival script

--- scripts/retrieve.py
+++ scripts/retrieve.py
@@ -1,10 +1,10 @@
 import argparse
 
-from configs.config import get_settings
+from configs.config import get_settings, Settings
 from src.pipeline.retrieval_pipeline import retrieve
 
-def run_retrieval(query: str, config_path: str = "configs/config.yaml"):
-    config = get_settings(config_path)
+def run_retrieval(query: str, config: Settings):
+    
     results = retrieve(config, query)
 
     print(f"\nQuery: {query}")
@@ -23,47 +23,11 @@ def run_retrieval(query: str, config_path: str = "configs/config.yaml"):
         print(result.chunk.text[:1500])
         print()
 
-# def run_retrieval():
-
-#     parser = argparse.ArgumentParser()
-
-#     parser.add_argument(
-#         "--config",
-#         default="configs/config.yaml",
-#     )
-
-#     parser.add_argument(
-#         "--query",
-#         required=True,
-#     )
-
-#     args = parser.parse_args()
-
-#     config = get_settings(
-#         args.config
-#     )
-
-#     results = retrieve(
-#         config,
-#         args.query,
-#     )
-
-#     print(f"\nQuery: {args.query}")
-#     print(f"Results: {len(results)}\n")
-
-#     for result in results:
-#             print("=" * 80)
-#             print(
-#                 f"Rank: {result.rank} "
-#                 f"Score: {result.score:.6f} "
-#                 f"Via: {result.retriever} "
-#                 f"ID: {result.chunk.chunk_id}"
-#             )
-#             print(result.metadata)
-#             print("-" * 80)
-#             print(result.chunk.text[:1500])
-#             print()
-
-
 if __name__ == "__main__":
-    run_retrieval(query="what are ducks?")
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--config", default="configs/config.yaml")
+    # parser.add_argument("--query", required=True)
+    # args = parser.parse_args()
+
+    config = get_settings(config_path="configs/config.yaml")
+    run_retrieval(query="what are ducks?", config=config)
