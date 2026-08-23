f69ca8a8751180e23136bffc35868fcabd85809c
Youssef <yousefkamel282@gmail.com>
2026-08-23 10:48:54 +0300

    added new plugins

--- src/plugins/registry_loader.py
+++ src/plugins/registry_loader.py
@@ -1,5 +1,7 @@
 def load_all_plugins():
     import src.chunking.recursive
+    import src.chunking.fixed
+    import src.chunking.sentence
     import src.embeddings.sentence_transformers
     import src.embeddings.ollama
     import src.vectorstores.faiss_store
